f36dfb5c163dee2bcd5d6bbf587a5297e07a13fd
pn8128 <1persongloria@gmail.com>
2021-06-16 17:54:38 +0900

    update

--- makeImage2.py
+++ makeImage2.py
@@ -2,26 +2,37 @@ from wordcloud import WordCloud, ImageColorGenerator
 import numpy as np
 from PIL import Image
 from random import randint
-#単語データを読み込む
-font="/Users/oishikota/Library/Fonts/Ronde-B_square.otf"
-with open("data3.txt") as f:
-    words=f.read()
+from datetime import datetime as dtdt
+# 単語データを読み込む
+font = "/System/Library/Fonts/Avenir.ttc"
+with open("data3.txt", "r", encoding="utf-8-sig") as f:
+    words = f.read()
 
-stop_words = [u'ある', u'こと', u'これ',u'peing',u'質問箱',u'RT',u'よう',u'そう',u'いる',u'もの',u'それ',u'ちゃん',u'さん',u'みたい',u'SleepMeister']
+stop_words = ["of", "for", "in", "the", "and", "by", "A", "on", "to"]
 
-def get_wordcrowd_color_mask( text, imgpath ):
-    img_color = np.array(Image.open( imgpath ))
-    wc = WordCloud(width=800,
-                   height=600,
+
+def get_wordcrowd_color_mask(text, imgpath):
+    freq = dict()
+    for w in words.split():
+        if w in stop_words:
+            continue
+        if w in freq:
+            freq[w] += 1
+        else:
+            freq[w] = 1
+    img_color = np.array(Image.open(imgpath))
+    wc = WordCloud(width=300,
+                   height=300,
                    font_path=font,
                    mask=img_color,
                    collocations=False,
                    stopwords=set(stop_words),
-                   background_color="#000000" #単語の重複しないように
-                  ).generate( text )
+                   background_color="#ffffff"  # 単語の重複しないように
+                   ).fit_words(freq)
 
     image_colors = ImageColorGenerator(img_color)
     wc.recolor(color_func=image_colors)
-    wc.to_file("tlNow.png")
+    wc.to_file("data/" + str(dtdt.now().timestamp()) + ".png")
 
-get_wordcrowd_color_mask(words, './hoge.png')
+
+get_wordcrowd_color_mask(words, './img/hoge.png')
